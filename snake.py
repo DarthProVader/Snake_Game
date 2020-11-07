82c16aac2bfb2a830e7391525cd7d744e8f563f2
DarthProVader <73229155+DarthProVader@users.noreply.github.com>
2020-11-07 15:53:53 +0100

    Update snake.py
    Updated - randomly generated food on the playing field and when snake eats it he gets longer

--- snake.py
+++ snake.py
@@ -1,44 +1,74 @@
+from random import randint
+
 def rady(pole):
     #funkce na převedení seznamů na string
     pole2 = []
     for seznam in pole:
-        pole2.append("".join(seznam))
+        pole2.append(" ".join(seznam))
     return print("\n".join(pole2))
 
 def nakresli_mapu(seznam):
     #přidá "X" na místo stanovené v seznamu
-    hraci_pole = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],]
     for el in range(len(seznam)):
         hraci_pole[seznam[el][0]][seznam[el][1]] = "X"
-    hraci_pole = rady(hraci_pole)
-    return hraci_pole
+    rady(hraci_pole)
+
+def del_souradnice():
+    hraci_pole[souradnice[0][0]][souradnice[0][1]] = "."
+    del souradnice[0]
 
 def pohyb(seznam, kompas):
-    #dostane souřadnice a světovou stranu, podle nich přidá "X" a odmaže poslední
+    #dostane souřadnice a světovou stranu, podle nich přidá souřadnici a odmaže poslední
     #pokud je již další souřadnice v seznamu vyhodí error, stejně tak pokud to bude záporné číslo = mimo mapu
     if kompas == "v":
-        if (seznam[-1][0], seznam[-1][1] + 1) not in seznam and (seznam[-1][1] + 1) >= 0:
-            souradnice.append((seznam[-1][0], seznam[-1][1] + 1))
+        vychod = (seznam[-1][0], seznam[-1][1] + 1) #proměnná na další políčko doprava
+        if vychod not in seznam and vychod[1] >= 0:
+            souradnice.append((seznam[-1][0], seznam[-1][1] + 1)) #pokud není v souřadnicích a není mimo mapu -> přidá se do souradnice
+            try:
+                if hraci_pole[vychod[0]][vychod[1]] != "?": #pokud je na políčku potrava prodlouží se
+                    del_souradnice()
+            except IndexError:
+                pass
         else:
             raise ValueError
     elif kompas == "j":
-        if (seznam[-1][0] + 1, seznam[-1][1]) not in seznam and (seznam[-1][0] + 1)  >= 0:
+        jih = (seznam[-1][0] + 1, seznam[-1][1]) #proměnná na další políčko dolu
+        if jih not in seznam and jih[1] >= 0:
             souradnice.append((seznam[-1][0] + 1, seznam[-1][1]))
+            try:
+                if hraci_pole[jih[0]][jih[1]] != "?":
+                    del_souradnice()
+            except IndexError:
+                pass
         else:
             raise ValueError
-    elif kompas == "s":
-        if (seznam[-1][0] - 1, seznam[-1][1]) not in seznam and (seznam[-1][0] - 1) >= 0:
+    elif kompas == "s": 
+        sever = (seznam[-1][0] - 1, seznam[-1][1]) #proměnná na další políčko nahoru
+        if sever not in seznam and sever[0] >= 0:
             souradnice.append((seznam[-1][0] - 1, seznam[-1][1]))
+            try:
+                if hraci_pole[sever[0]][sever[1]] != "?":
+                    del_souradnice()
+            except IndexError:
+                pass
         else:
             raise ValueError
     elif kompas == "z":
-        if (seznam[-1][0] , seznam[-1][1] - 1) not in seznam and (seznam[-1][1] - 1) >= 0:
+        zapad = (seznam[-1][0] , seznam[-1][1] - 1) #proměnná na další políčko doleva
+        if zapad not in seznam and zapad[1] >= 0:
             souradnice.append((seznam[-1][0] , seznam[-1][1] - 1))
+            try:
+                if hraci_pole[zapad[0]][zapad[1]] != "?":
+                    del_souradnice()
+            except IndexError:
+                pass
         else:
             raise ValueError
-    del souradnice[0]
 
-souradnice = [(0, 0), (1, 0), (2, 0)]
+souradnice = [(0, 0), (1, 0), (2, 0)] #had
+hraci_pole = [['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],['.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],]
+hraci_pole[randint(3, 10)][randint(0, 10)] = "?" #hadí potrava
+
 while True:
     smer = "p"
     while smer not in ["s", "v", "j", "z"]:
